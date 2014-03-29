d76d025054b2b7d4862fae3332222fc7efc1ba1a
JongChan Choi <disjukr@naver.com>
2014-03-30 00:55:50 +0900

    api mockup

--- cellophane.js
+++ cellophane.js
@@ -1,11 +1,37 @@
 var Cellophane = (function () {
+  var vertexShaderSource = [
+    ''
+  ].join('\n');
   function Cellophane() {
     var self = this;
     var gl;
     self.domElement = document.createElement('canvas');
     gl = initWebGL(self.domElement);
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    self.render = function () {
+      // clear first
+      gl.clearColor(0, 0, 0, 0);
+      gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+    self.layers = [];
+  }
+  Cellophane.BlendMode = {
+    NORMAL: 'normal'
+  };
+  Cellophane.Layer = function (content, blendMode, opacity, visibility) {
+    var self = this;
+    self.content = content || null;
+    self.blendMode = blendMode || Cellophane.BlendMode.NORMAL;
+    self.opacity = (typeof opacity === 'undefined') ? 1.0 : opacity;
+    self.visibility = (typeof visibility === 'undefined') ? true : visibility;
+  };
+  function prepareShader(gl, source, vertex) {
+    var shaderType = vertex ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER;
+    var shader = gl.createShader(shaderType);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+      throw gl.getShaderInfoLog(shader);
+    return shader;
   }
   function initWebGL(canvas) {
     var gl = null;
